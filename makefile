c002d88daa8c2cfe5d9da460a5dbce3df7f4e321
Straumann Till <till.straumann@psi.ch>
2020-04-23 10:23:03 +0200

    utils.mk: record git tag/version in a version file

--- makefile
+++ makefile
@@ -3,7 +3,7 @@ HERE:=$(dir $(lastword $(MAKEFILE_LIST)))
 include /ioc/tools/driver.makefile
 include $(HERE)utils.mk
 
-MKSRCS+=utils.mk
+MKSRCS+=utils.mk makeUtils_version.mk
 
 $(MODULE_LOCATION)/%.mk: %.mk
 	$(INSTALL) -D -m 0644 $< $@
@@ -22,8 +22,23 @@ ifdef INSTALL_MODULE_TOP_RULE
 $(INSTALL_MODULE_TOP_RULE) $(MKINSTALLS)
 endif
 
-.PHONY: instdebug
+ifndef BUILDRULE
+BUILDRULE=build::
+endif
 
 instdebug:
 	echo "LIBVERSION $(LIBVERSION)"
 	echo "MODULE_LOCATION $(MODULE_LOCATION)"
+
+#$(BUILDRULE) check-makepass
+
+check-makepass:
+	@if [ -z "$(MAKE_PASS)" ] ; then echo "ERROR: MAKE_PASS undefined"; exit 1; fi
+	@echo "MAKE_PASS: $(MAKE_PASS)"
+
+.PHONY: instdebug check-makepass
+
+
+foo:
+	echo '$(LIBVERSION)'
+	echo '$(call CONVERT_VERSION_NUMBER,test,4)'
--- utils.mk
+++ utils.mk
@@ -7,6 +7,12 @@
 # version.
 #
 
+
+# record where we are
+_MAKE_UTILS_HERE:=$(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(_MAKE_UTILS_HERE)makeUtils_version.mk
+
 #
 # Find highest version in a path
 #
@@ -32,6 +38,40 @@ endef
 ifndef INSTALL
 INSTALL=install
 endif
+
+#
+# Define a variable that communicates the 'pass' of driver.makefile
+#
+ifndef MAKE_PASS
+ifndef EPICSVERSION
+MAKE_PASS=1
+else ifndef T_A
+MAKE_PASS=2
+else ifeq ($(filter O.%,$(notdir $(CURDIR))),)
+MAKE_PASS=3
+else
+MAKE_PASS=4
+endif
+endif # ifndef MAKE_PASS
+
+# Convert a version string (numericals separated by dots)
+# into a single number (which is is easier to compare)
+#
+# call this function with ( version_string, max_level )
+#
+# the 'max_level' indicates how many levels (major, minor, ...)
+# are included in the numerical value. Levels are 'separated'
+# by factors of 100.
+#
+# E.g., $(call CONVERT_VERSION_NUMBER, 1.2.3, 2)
+# yields:
+#
+#    102
+#
+define CONVERT_VERSION_NUMBER
+$(shell echo '$1' | awk 'BEGIN{ FS="." }/^([0-9]+[.])*[0-9]+$$/{ v = 0; m =$$NF < $2 ? $$NF : $2; for (i=1; i<=m; i=i+1) v = 100*v + i; for (   ; i<= $2;  i=i+1) v = 100*v; print v; }')
+endef
+
 
 # If you want to install something into
 # the module's top area (e.g., docs) then
@@ -105,6 +145,11 @@ endif
 	echo "  $(GIT_REMOTE)" >> $@
 
 install-gitinfo: $(MODULE_GITINFO)
+
+# Give a 'test' version the number 99.0.0.0
+%_version.mk: utils.mk
+	$(RM) $@
+	echo 'MAKEUTILS_VERSION=$(or $(MAKEUTILS_MANUAL_VERSION),$(call CONVERT_VERSION_NUMBER,$(LIBVERSION),4),99000000)' >> $@
 
 .PHONY: .FORCE install-gitinfo
 
