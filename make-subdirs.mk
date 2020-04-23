3a0994f0c191bb3e4f3602cede83b113d3381bfc
Straumann Till <till.straumann@psi.ch>
2020-04-23 14:23:12 +0200

    make-subdirs.mk: fixed shell syntax

--- make-subdirs.mk
+++ make-subdirs.mk
@@ -3,6 +3,6 @@ default:
 TGTS=default $(filter-out default,$(MAKECMDGOALS))
 
 $(TGTS): .FORCE
-	for d in $(SUBDIRS) do; $(MAKE) -C $d $@; done
+	for d in $(SUBDIRS); do $(MAKE) -C $$d $@; done
 
 .PHONY: .FORCE $(TGTS)
